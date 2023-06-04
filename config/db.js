0097f5b5ff867990acca339e7839f9fa74917c74
SigmaAjax <jankennny@seznam.cz>
2023-06-04 16:28:02 +0200

    configuration for heroku db added

--- config/db.js
+++ config/db.js
@@ -3,33 +3,69 @@ require('dotenv').config();
 
 const postDbPool = mysql.createPool({
 	connectionLimit: 10,
-	host: process.env.DB_HOST,
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
+	host:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).hostname
+			: process.env.DB_HOST,
+	user:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).username
+			: process.env.DB_USERNAME,
+	password:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).password
+			: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE_POST,
-	port: process.env.DB_PORT,
+	port:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).port
+			: process.env.DB_PORT,
 	waitForConnections: true,
 	queueLimit: 0,
 });
 
 const albumDbPool = mysql.createPool({
 	connectionLimit: 10,
-	host: process.env.DB_HOST,
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
+	host:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).hostname
+			: process.env.DB_HOST,
+	user:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).username
+			: process.env.DB_USERNAME,
+	password:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).password
+			: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE_ALBUMS,
-	port: process.env.DB_PORT,
+	port:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).port
+			: process.env.DB_PORT,
 	waitForConnections: true,
 	queueLimit: 0,
 });
 
 const employeesDbPool = mysql.createPool({
 	connectionLimit: 10,
-	host: process.env.DB_HOST,
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
+	host:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).hostname
+			: process.env.DB_HOST,
+	user:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).username
+			: process.env.DB_USERNAME,
+	password:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).password
+			: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE_EMPLOYEES,
-	port: process.env.DB_PORT,
+	port:
+		process.env.NODE_ENV === 'production'
+			? new URL(process.env.CLEARDB_DATABASE_URL).port
+			: process.env.DB_PORT,
 	waitForConnections: true,
 	queueLimit: 0,
 });
