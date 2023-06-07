83a1993d6d2252a82fe1ef7acdfc86fb30ebf729
SigmaAjax <jankennny@seznam.cz>
2023-06-07 21:10:45 +0200

    changed the CLEARDB_DATABASE_URL to CLEARDB_CYAN_URL

--- config/db.js
+++ config/db.js
@@ -5,20 +5,20 @@ const postDbPool = mysql.createPool({
 	connectionLimit: 10,
 	host:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).hostname
+			? new URL(process.env.CLEARDB_CYAN_URL).hostname
 			: process.env.DB_HOST,
 	user:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).username
+			? new URL(process.env.CLEARDB_CYAN_URL).username
 			: process.env.DB_USERNAME,
 	password:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).password
+			? new URL(process.env.CLEARDB_CYAN_URL).password
 			: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE_POST,
 	port:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).port
+			? new URL(process.env.CLEARDB_CYAN_URL).port
 			: process.env.DB_PORT,
 	waitForConnections: true,
 	queueLimit: 0,
@@ -28,20 +28,20 @@ const albumDbPool = mysql.createPool({
 	connectionLimit: 10,
 	host:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).hostname
+			? new URL(process.env.CLEARDB_CYAN_URL).hostname
 			: process.env.DB_HOST,
 	user:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).username
+			? new URL(process.env.CLEARDB_CYAN_URL).username
 			: process.env.DB_USERNAME,
 	password:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).password
+			? new URL(process.env.CLEARDB_CYAN_URL).password
 			: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE_ALBUMS,
 	port:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).port
+			? new URL(process.env.CLEARDB_CYAN_URL).port
 			: process.env.DB_PORT,
 	waitForConnections: true,
 	queueLimit: 0,
@@ -51,20 +51,20 @@ const employeesDbPool = mysql.createPool({
 	connectionLimit: 10,
 	host:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).hostname
+			? new URL(process.env.CLEARDB_CYAN_URL).hostname
 			: process.env.DB_HOST,
 	user:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).username
+			? new URL(process.env.CLEARDB_CYAN_URL).username
 			: process.env.DB_USERNAME,
 	password:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).password
+			? new URL(process.env.CLEARDB_CYAN_URL).password
 			: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE_EMPLOYEES,
 	port:
 		process.env.NODE_ENV === 'production'
-			? new URL(process.env.CLEARDB_DATABASE_URL).port
+			? new URL(process.env.CLEARDB_CYAN_URL).port
 			: process.env.DB_PORT,
 	waitForConnections: true,
 	queueLimit: 0,
